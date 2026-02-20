2cc68ea427b690faea48579a67a5bd82060cd1db
Sa1i3rii <aomsinwarit@gmail.com>
2026-02-20 18:18:12 +0700

    7

--- src/practicals/p03/warrior.ts
+++ src/practicals/p03/warrior.ts
@@ -1,5 +1,22 @@
 import { Character } from "./character";
 
 export class Warrior extends Character {
-  // TODO: implement class properties, constructor with super(...), and methods
+    private weapon: string;
+
+    constructor(name: string, weapon: string, health: number = 100) {
+        super(name, health);
+        this.weapon = weapon;
+    }
+
+    getWeapon(): string {
+        return this.weapon;
+    }
+
+    receiveDamage(damage: number): void {
+
+        const reducedDamage = damage * 0.9;
+
+
+        super.receiveDamage(reducedDamage);
+    }
 }
